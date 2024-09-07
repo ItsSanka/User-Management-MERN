02dae329a0cc3519dfeb2d569d93ed0dd6e31f99
ItsSanka <itssanka97@gmail.com>
2024-09-07 18:00:26 +0530

    Contact page

--- frontend/src/Components/ContactUs/ContactUs.jsx
+++ frontend/src/Components/ContactUs/ContactUs.jsx
@@ -29,7 +29,7 @@ function ContactUs() {
   return (
     <div>
       <NavBar></NavBar>
-      <h1>Contact Us</h1>
+      <h1>Contact Me</h1>
       <form ref={form} onSubmit={sendEmail}>
         <label>Name</label>
         <input type="text" name="from_name" />
